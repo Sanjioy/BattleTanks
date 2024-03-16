17d8ff64c4455a566c760ffbee5e3ece075221c0
Sanjioy <127838002+Sanjioy@users.noreply.github.com>
2024-03-16 21:42:35 +0300

    5. Interaction with obstacles

--- app/src/main/java/com/example/battletanks/drawers/ElementsDrawer.kt
+++ app/src/main/java/com/example/battletanks/drawers/ElementsDrawer.kt
@@ -3,8 +3,12 @@ package com.example.battletanks.drawers
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
+import androidx.core.view.marginLeft
+import androidx.core.view.marginTop
 import com.example.battletanks.CELL_SIZE
 import com.example.battletanks.R
+import com.example.battletanks.binding
+import com.example.battletanks.enums.Direction
 import com.example.battletanks.enums.Material
 import com.example.battletanks.models.Coordinate
 import com.example.battletanks.models.Element
@@ -38,5 +42,82 @@ class ElementsDrawer(val container: FrameLayout) {
         view.layoutParams = layoutParams
         container.addView(view)
         elementsOnContainer.add(Element(viewId, currentMaterial, coordinate))
+    }
+
+    fun move(myTank: View, direction: Direction) {
+        val layoutParams = myTank.layoutParams as FrameLayout.LayoutParams
+        val currentCoordinate = Coordinate(layoutParams.topMargin, layoutParams.leftMargin)
+        when(direction) {
+            Direction.UP -> {
+                myTank.rotation = 0f
+                (myTank.layoutParams as FrameLayout.LayoutParams).topMargin += -CELL_SIZE
+            }
+
+            Direction.DOWN -> {
+                myTank.rotation = 180f
+                (myTank.layoutParams as FrameLayout.LayoutParams).topMargin += CELL_SIZE
+            }
+
+            Direction.LEFT -> {
+                myTank.rotation = 270f
+                (myTank.layoutParams as FrameLayout.LayoutParams).leftMargin -= CELL_SIZE
+            }
+
+            Direction.RIGHT -> {
+                myTank.rotation = 90f
+                (myTank.layoutParams as FrameLayout.LayoutParams).leftMargin += CELL_SIZE
+            }
+        }
+
+        val nextCoordinate = Coordinate(layoutParams.topMargin, layoutParams.leftMargin)
+        if (checkTankCanMoveThroughBorder(
+            nextCoordinate,
+            myTank
+            ) && checkTankCanMoveThroughMaterial(nextCoordinate))
+        {
+            binding.container.removeView(binding.myTank)
+            binding.container.addView(binding.myTank)
+        } else {
+            (myTank.layoutParams as FrameLayout.LayoutParams).topMargin = currentCoordinate.top
+            (myTank.layoutParams as FrameLayout.LayoutParams).leftMargin = currentCoordinate.left
+        }
+    }
+
+    private fun getElementyCoordinates(coordinate: Coordinate) =
+        elementsOnContainer.firstOrNull { it.coordinate == coordinate }
+
+    private fun  checkTankCanMoveThroughMaterial(coordinate: Coordinate): Boolean {
+        getTankCoordinates(coordinate).forEach {
+            val element = getElementyCoordinates(it)
+            if (element != null && !element.material.tankConGoThrough) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun checkTankCanMoveThroughBorder(coordinate: Coordinate, myTank: View): Boolean {
+        if (coordinate.top >= 0 &&
+            coordinate.top + myTank.height <= binding.container.height &&
+            coordinate.left >= 0 &&
+            coordinate.left + myTank.width <= binding.container.width
+            ) {
+            return true
+        }
+        return false
+    }
+
+    private fun getTankCoordinates(topLeftCoordinate: Coordinate): List<Coordinate> {
+        val coordinateList = mutableListOf<Coordinate>()
+        coordinateList.add(topLeftCoordinate)
+        coordinateList.add(Coordinate(topLeftCoordinate.top + CELL_SIZE, topLeftCoordinate.left))
+        coordinateList.add(Coordinate(topLeftCoordinate.top, topLeftCoordinate.left + CELL_SIZE))
+        coordinateList.add(
+            Coordinate(
+                topLeftCoordinate.top + CELL_SIZE,
+                topLeftCoordinate.left + CELL_SIZE
+            )
+        )
+        return coordinateList
     }
 }
