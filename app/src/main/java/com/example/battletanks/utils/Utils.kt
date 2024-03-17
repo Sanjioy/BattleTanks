1446718bbc43ec534cafd83187ed2ba1002f45bc
Sanjioy <127838002+Sanjioy@users.noreply.github.com>
2024-03-17 18:14:24 +0300

    11. Making a base for tanks

--- app/src/main/java/com/example/battletanks/utils/Utils.kt
+++ app/src/main/java/com/example/battletanks/utils/Utils.kt
@@ -1,6 +1,7 @@
 package com.example.battletanks.utils
 
 import android.view.View
+import com.example.battletanks.CELL_SIZE
 import com.example.battletanks.binding
 import com.example.battletanks.models.Coordinate
 import com.example.battletanks.models.Element
@@ -13,5 +14,22 @@ fun View.checkViewCanMoveThrounghBorder(coordinate: Coordinate): Boolean {
 
 }
 
-fun getElementByCoordinates(coordinate: Coordinate, elementsOnContainer: List<Element>) =
-    elementsOnContainer.firstOrNull { it.coordinate == coordinate }
+fun getElementByCoordinates(
+    coordinate: Coordinate,
+    elementsOnContainer: List<Element>
+): Element? {
+    for (element in elementsOnContainer) {
+        for (height in 0 until element.height) {
+            for (width in 0 until element.width) {
+                val searchingCoordinate = Coordinate(
+                    top = element.coordinate.top + height * CELL_SIZE,
+                    left = element.coordinate.left + width * CELL_SIZE
+                )
+                if (coordinate == searchingCoordinate) {
+                    return element
+                }
+            }
+        }
+    }
+    return null
+}
