33a1b850c0741604ae76d2eef0ac7728ef0ac217
Sanjioy <127838002+Sanjioy@users.noreply.github.com>
2024-03-17 20:14:15 +0300

    16. Redoing the creation of respawns

---
+++ app/src/main/java/com/example/battletanks/drawers/EnemyDrawer.kt
@@ -0,0 +1,22 @@
+package com.example.battletanks.drawers
+
+import android.widget.FrameLayout
+import com.example.battletanks.CELL_SIZE
+import com.example.battletanks.models.Coordinate
+import kotlin.reflect.KDeclarationContainer
+
+class EnemyDrawer(private val container: FrameLayout) {
+    private val respawnList: List<Coordinate>
+
+    init {
+        respawnList = getRespawnList()
+    }
+
+    private fun getRespawnList(): List<Coordinate> {
+        val respawnList = mutableListOf<Coordinate>()
+        respawnList.add(Coordinate(0, 0))
+        respawnList.add(Coordinate(0, container.width / 2 - CELL_SIZE))
+        respawnList.add(Coordinate(0, container.width - CELL_SIZE))
+        return respawnList
+    }
+}
